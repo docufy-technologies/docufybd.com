b2002c49f33c35e4980f405483a28850dac91b8b
Rakibul Hasan Ratul <rakibulhasanratul@proton.me>
2026-09-17 15:02:00 +0600

    feat: configure background and /faq page

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,5 +5,15 @@ import "../styles.css";
 export const Route = createRootRoute({ component: RootComponent });
 
 function RootComponent() {
-  return <Outlet />;
+  return (
+    <div class="relative min-h-dvh w-full overflow-hidden bg-background">
+      <div
+        aria-hidden="true"
+        class="pointer-events-none fixed inset-0 z-0 bg-[url('/background.jpg')] bg-cover bg-center bg-no-repeat bg-fixed"
+      />
+      <div class="relative z-10 h-screen w-screen overflow-y-auto">
+        <Outlet />
+      </div>
+    </div>
+  );
 }
