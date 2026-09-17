1562b070a5d8a274a0e70024cc8f91f517df2577
Rakibul Hasan Ratul <rakibulhasanratul@proton.me>
2026-09-17 16:07:05 +0600

    feat: embed footer to __root

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { createRootRoute, Outlet } from "@tanstack/solid-router";
 
 import "../styles.css";
+import Footer from "~/components/blocks/footer";
 
 export const Route = createRootRoute({ component: RootComponent });
 
@@ -11,8 +12,9 @@ function RootComponent() {
         aria-hidden="true"
         class="pointer-events-none fixed inset-0 z-0 bg-[url('/background.jpg')] bg-cover bg-center bg-no-repeat bg-fixed"
       />
-      <div class="relative z-10 h-screen w-screen overflow-y-auto">
+      <div class="relative z-10 h-screen w-screen overflow-y-auto flex flex-col">
         <Outlet />
+        <Footer />
       </div>
     </div>
   );
